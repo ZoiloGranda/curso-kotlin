d7904b06ca3119e098dcbc029c854c12e23cf2f7
Zoilo Granda <zgranda@gmail.com>
2023-12-22 18:31:22 -0400

    initial commit

---
+++ app/src/main/java/com/example/androidtestapp/Variables.kt
@@ -0,0 +1,17 @@
+package com.example.androidtestapp
+
+fun main() {
+    val age:Int = 20
+    var age2:Int = 20
+    println(age2)
+    age2 = 25
+    println(age2)
+    val example:Long = 9938238123
+    val floatExample:Float=10.5F
+    val doubleExample:Double = 29.1224
+    val charExample:Char ='Z'
+    val charExample1:Char ='?'
+    val stringExample: String = "zoilo granda"
+    val booleanExample: Boolean = false
+    print(charExample)
+}
