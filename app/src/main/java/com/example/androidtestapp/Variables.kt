0103a08ca3124c84f155195d525d9d5aa37fa6af
Zoilo Granda <zgranda@gmail.com>
2023-12-28 22:34:12 -0400

    funciones

--- app/src/main/java/com/example/androidtestapp/Variables.kt
+++ app/src/main/java/com/example/androidtestapp/Variables.kt
@@ -1,6 +1,39 @@
 package com.example.androidtestapp
 
 fun main() {
+    variablesNumericas()
+    showMyAge(currentAge = 33)
+    add(firstNumber = 25, secondNumber = 25)
+    showMyName(name = "Zoilo Granda")
+    val myResta = restar(firstNumber = 10, secondNumber = 5)
+    println(myResta)
+    val charExample:Char ='Z'
+    val charExample1:Char ='?'
+    val stringExample: String = "zoilo granda"
+    val stringExample2: String = "AAAAA"
+    val stringExample3: String = "BBBBB"
+    println(stringExample2+stringExample3)
+    var stringConcatenada = "Hola me llamo $stringExample"
+    println(stringConcatenada)
+}
+
+fun showMyName(name:String){
+    println("my name is $name")
+}
+
+fun add(firstNumber: Int, secondNumber:Int){
+    println(firstNumber + secondNumber)
+}
+
+fun restar(firstNumber: Int, secondNumber: Int):Int{
+    return firstNumber - secondNumber
+}
+
+fun showMyAge(currentAge:Int){
+    println("Tengo $currentAge años")
+}
+
+fun variablesNumericas(){
     val age:Int = 20
     var age2:Int = 20
     println(age2)
@@ -9,19 +42,14 @@ fun main() {
     val example:Long = 9938238123
     val floatExample:Float=10.5F
     val doubleExample:Double = 29.1224
-    val charExample:Char ='Z'
-    val charExample1:Char ='?'
-    val stringExample: String = "zoilo granda"
-    val stringExample2: String = "AAAAA"
-    val stringExample3: String = "BBBBB"
-    val booleanExample: Boolean = false
     println(age + age2)
     println(age - age2)
     println(age + example)
     var exampleSuma = age + example.toInt()
     println(exampleSuma)
-    println(stringExample2+stringExample3)
-    var stringConcatenada = "Hola me llamo $stringExample"
-    println(stringConcatenada)
+}
 
+fun variablesBoolean(){
+    val booleanExample: Boolean = false
+    println(booleanExample)
 }
