0f092a2cdc68620e0b6f18eb94d1b7c3e04e0ac0
Zoilo Granda <zgranda@gmail.com>
2023-12-30 14:25:45 -0400

    variables

--- app/src/main/java/com/example/androidtestapp/ifElse.kt
+++ app/src/main/java/com/example/androidtestapp/ifElse.kt
@@ -1,14 +1,37 @@
 package com.example.androidtestapp
 
-fun main(){
-ifBasico()
+fun main() {
+    ifMultiple()
 }
 
-fun ifBasico(){
+fun ifBasico() {
     val name = "Zoilo"
-    if(name == "Pepe"){
+    if (name == "Pepe") {
         println("IF")
     } else {
         println("name is $name")
+    }
+}
+
+fun ifMultiple(){
+    var age = 18
+    var permission = false
+    if(age >= 18 && permission){
+        println("a tomar cerveza")
+    } else{
+        println("no puedo")
+    }
+}
+
+fun ifAnidado() {
+    val animal = "asd"
+    if(animal== "Dog"){
+        println("Es un perro")
+    } else if (animal == "Cat"){
+        println("es un gato")
+    } else if (animal == "Bird"){
+    println("es un pajaro")
+} else{
+    println("no es ninguno de los 3")
     }
 }
