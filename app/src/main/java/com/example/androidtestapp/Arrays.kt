fed9e36309f5790ecee4b3f2761e6526ff2419fc
Zoilo Granda <zgranda@gmail.com>
2024-01-04 15:03:12 -0400

    arrays

---
+++ app/src/main/java/com/example/androidtestapp/Arrays.kt
@@ -0,0 +1,20 @@
+package com.example.androidtestapp
+
+fun main(){
+    val weekDays = arrayOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+    println(weekDays.size)
+    println(weekDays[4])
+    weekDays[1]="Zoilo"
+    println(weekDays[1])
+
+    for (position in weekDays.indices){
+        println(weekDays[position])
+    }
+    for((position,value) in weekDays.withIndex()){
+        println("position $position es $value")
+    }
+
+    for (day in weekDays){
+        println("day es $day")
+    }
+}
