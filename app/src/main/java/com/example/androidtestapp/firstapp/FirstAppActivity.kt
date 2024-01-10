677863a6d215f2ed8772c66c638a6bd36aef47b2
Zoilo Granda <zgranda@gmail.com>
2024-01-10 17:44:24 -0400

    button listener

--- app/src/main/java/com/example/androidtestapp/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/androidtestapp/firstapp/FirstAppActivity.kt
@@ -2,11 +2,17 @@ package com.example.androidtestapp.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 import com.example.androidtestapp.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            Log.i("taag", "presionado")
+        }
     }
 }
