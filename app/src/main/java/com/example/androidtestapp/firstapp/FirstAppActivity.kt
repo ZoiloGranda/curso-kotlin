e036d937ce86077113539d59c45d1ba3a6d11184
Zoilo Granda <zgranda@gmail.com>
2024-01-11 15:37:23 -0400

    result activity

--- app/src/main/java/com/example/androidtestapp/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/androidtestapp/firstapp/FirstAppActivity.kt
@@ -1,8 +1,10 @@
 package com.example.androidtestapp.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
 import com.example.androidtestapp.R
 
@@ -11,8 +13,14 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<EditText>(R.id.etName)
         btnStart.setOnClickListener {
-            Log.i("taag", "presionado")
+            val name = etName.text.toString()
+            if(name.isNotEmpty()){
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME",name)
+                startActivity(intent)
+            }
         }
     }
 }
