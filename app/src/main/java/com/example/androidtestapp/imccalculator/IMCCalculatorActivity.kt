328c4c2e8d26006395a24a94f99c49915cfabe97
Zoilo Granda <zgranda@gmail.com>
2024-01-13 15:59:44 -0400

    imc initial commit

---
+++ app/src/main/java/com/example/androidtestapp/imccalculator/IMCCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidtestapp.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidtestapp.R
+
+class IMCCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imccalculator)
+    }
+}
